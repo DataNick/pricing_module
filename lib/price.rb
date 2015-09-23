cbb54d66c75f30b18cbce1470286611fb61a8810
DataNick <DataNick@users.noreply.github.com>
2015-09-23 17:50:49 -0400

    Second system for calculating markups based on materials

--- lib/price.rb
+++ lib/price.rb
@@ -22,3 +22,31 @@
 		flat_markup(cost)*0.02
 	end
 
+	# system 2
+	# Another system calculates the base price depending on the number of products needing to packaged.
+	# Markup calculator needs to accept initial base price AND different categories of markups AND calculate final cost
+	# Flat markup is calculated first; all other markups calculated on top of the (base price + flat markup)
+
+	def add_tax(product, people)
+		case product
+		when 1 then 0.02 + (people * 0.012)
+		when 2 then 0.075 + (people*0.012)
+		when 3 then 0.13 + (people * 0.012)
+		when 0 then people*0.012
+		when 7 
+			return
+		end
+	end
+
+
+	def markup_calc(base_price, num_people, category)
+		case category
+		when 1 then puts (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
+		when 2 then puts (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
+		when 3 then puts (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
+		when 4 then puts (flat_markup(base_price) * (1+add_tax(category, num_people))).round(2)
+		else puts "Please make a selection"
+		end
+	end
+
+markup_calc(1299.99, 3, 3)
