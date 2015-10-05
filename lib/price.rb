e8286f9fe7864731fa010006060e4ffb9e445d6c
DataNick <DataNick@users.noreply.github.com>
2015-10-05 18:41:09 -0400

    Created Gemfile

--- lib/price.rb
+++ lib/price.rb
@@ -4,7 +4,7 @@ class Product
   DRUG_RATE = 0.075
   ELECTRONICS_RATE = 0.02
   PEOPLE_RATE = 0.012
-  FLAT_RATE = 0.05
+  FLAT_MARKUP_RATE = 0.05
   ZERO_MARKUP = 0
 
 	attr_accessor :price, :people_required, :category
@@ -15,19 +15,9 @@ class Product
     @category = category
   end
 
-	# def validate_price
- #    if @price.is_a?(Float) || @price.is_a?(Integer)
- #      if (@price != nil)
- #        flat_markup
- #      end
- #    else
- #      raise(ArgumentError, "Price must be either a float or an integer")
- #    end
- #  end
-
   def flat_markup
     # validate_price
-    @price + (@price * FLAT_RATE)
+    @price + (@price * FLAT_MARKUP_RATE)
   end
 
   def people_markup
@@ -44,94 +34,48 @@ class Product
       end
   end
 
-	def number_of_people_rate
-		if @number_of_people.is_a?(Integer) && @number_of_people >= 0
-			people_markup
-		else
-			raise(ArgumentError, "Must pass in a number")
-		end
-	end
-
-	def assert_valid_people_input
-		if @number_of_people.is_a?(Integer)
-			if @number_of_people < 0
-				raise ArgumentError
-			else
-				@number_of_people
-			end
-		else
-			raise ArgumentError
-		end
-	end
-
-	def assert_valid_product_input
-		if !@category.is_a?(String)
-			raise(ArgumentError, "Category must be a string")
-		else
-			@category
-		end
-	end
-
-  # def pharmaceuticals_markup(cost)
-  #   if cost.is_a?(Float) || cost.is_a?(Integer)
-  #     if cost != nil
-  #       (flat_markup(cost)*0.075).round(2)
-  #     end
-  #   else
-  #     raise(ArgumentError, "Must pass in price as a Float or Integer")
-  #   end
-  # end
-
-  # def food_markup(cost)
-  #   if cost.is_a?(Float) || cost.is_a?(Integer)
-  #     if cost != nil
-  #       (flat_markup(cost)*0.13).round(2)
-  #     end
-  #   else
-  #     raise ArgumentError
-  #   end
-  # end
-
-  # def electronics_markup(cost)
-  #   if cost.is_a?(Float) || cost.is_a?(Integer)
-  #     if cost != nil
-  #       (flat_markup(cost)*0.02).round(2)
-  #     end
-  #   else
-  #     raise ArgumentError
-  #   end
-  # end
-
-
-
-
-	def add_tax(product, people)
-		product_checker(product)
-		people_checker(people)
-		if product == "food"
-			FOOD_RATE + (for_each_person(people))
-		elsif product == "drugs"
-			DRUG_RATE + (for_each_person(people))
-		elsif product == "electronics"
-			ELECTRONICS_RATE + (for_each_person(people))
-		else
-			for_each_person(people)
-		end
-	end
-
-	# def markup_calc(base_price, num_people, category)
-	# 	case @category
-	# 	when "food" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2) #removed puts since tests were reading the nil value as the returned value
-	# 	when "drugs" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
-	# 	when "electronics" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
-	# 	when /.*/ then (flat_markup(base_price) * (1+add_tax(category, num_people))).round(2)
-	# 	else puts "Not a valid input for category"
-	# 	end
-	# end
-
   def total_price
     total_price_with_all_markups = (flat_markup + people_markup + category_markup).round(2)
   end
+  # def validate_price
+ #    if @price.is_a?(Float) || @price.is_a?(Integer)
+ #      if (@price != nil)
+ #        flat_markup
+ #      end
+ #    else
+ #      raise(ArgumentError, "Price must be either a float or an integer")
+ #    end
+ #  end
+
+  # def number_of_people_rate
+  #   if @number_of_people.is_a?(Integer) && @number_of_people >= 0
+  #     people_markup
+  #   else
+  #     raise(ArgumentError, "Must pass in a number")
+  #   end
+  # end
+
+  # def assert_valid_people_input
+  #   if @number_of_people.is_a?(Integer)
+  #     if @number_of_people < 0
+  #       raise ArgumentError
+  #     else
+  #       @number_of_people
+  #     end
+  #   else
+  #     raise ArgumentError
+  #   end
+  # end
+
+  # def assert_valid_product_input
+  #   if !@category.is_a?(String)
+  #     raise(ArgumentError, "Category must be a string")
+  #   else
+  #     @category
+  #   end
+  # end
+
+
 
 end
 
