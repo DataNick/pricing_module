03ffbf386933ab607ae9be1c932bddc12dfa5221
DataNick <DataNick@users.noreply.github.com>
2015-10-05 13:06:15 -0400

    Refactored code and got all inputs to match outputs

--- lib/price.rb
+++ lib/price.rb
@@ -5,139 +5,146 @@ class Product
   ELECTRONICS_RATE = 0.02
   PEOPLE_RATE = 0.012
   FLAT_RATE = 0.05
+  ZERO_MARKUP = 0
 
 	attr_accessor :price, :people_required, :category
 
-  def initialize(price, people_required, category)
+  def initialize(price, number_of_people, category)
     @price = price
-    @people_required = people_required
+    @number_of_people= number_of_people
     @category = category
   end
 
-	def validate_price(input)
-    if @price.is_a?(Float) || @price.is_a?(Integer)
-      if (@price != nil)
-        flat_markup
-      end
-    else
-      raise(ArgumentError, "Price must be either a float or an integer")
-    end
-  end
+	# def validate_price
+ #    if @price.is_a?(Float) || @price.is_a?(Integer)
+ #      if (@price != nil)
+ #        flat_markup
+ #      end
+ #    else
+ #      raise(ArgumentError, "Price must be either a float or an integer")
+ #    end
+ #  end
 
   def flat_markup
+    # validate_price
     @price + (@price * FLAT_RATE)
   end
 
   def people_markup
-    @people * PEOPLE_RATE
+    people_markup = flat_markup * (@number_of_people * PEOPLE_RATE)
   end
 
   def category_markup
     category_markup = case @category
-      when "drugs" then flat_markup * DRUG
-      when "food" then flat_markup * FOOD
-      when "electronics" then flat_markup * ELECTRONICS
+      when "drugs" then flat_markup * DRUG_RATE
+      when "food" then flat_markup * FOOD_RATE
+      when "electronics" then flat_markup * ELECTRONICS_RATE
       else
-        0
+        ZERO_MARKUP
       end
   end
 
-	# def flat_markup(cost)
-	# 	if cost.is_a?(Float) || cost.is_a?(Integer)
-	# 		if (cost != nil && cost != 0)
-	# 			(cost*(1+0.05)).round(2)
-	# 		end
-	# 	else
-	# 		raise ArgumentError
-	# 	end
-	# end
-
-	def number_of_people_rate(number)
-		if number.is_a?(Integer) && number >= 0
-			(number * PEOPLE_RATE)
+	def number_of_people_rate
+		if @number_of_people.is_a?(Integer) && @number_of_people >= 0
+			people_markup
 		else
 			raise(ArgumentError, "Must pass in a number")
 		end
 	end
 
-
-	# def pharmaceuticals_markup(cost)
-	# 	if cost.is_a?(Float) || cost.is_a?(Integer)
-	# 		if cost != nil
-	# 			(flat_markup(cost)*0.075).round(2)
-	# 		end
-	# 	else
-	# 		raise(ArgumentError, "Must pass in price as a Float or Integer")
-	# 	end
-	# end
-
-	# def food_markup(cost)
-	# 	if cost.is_a?(Float) || cost.is_a?(Integer)
-	# 		if cost != nil
-	# 			(flat_markup(cost)*0.13).round(2)
-	# 		end
-	# 	else
-	# 		raise ArgumentError
-	# 	end
-	# end
-
-	# def electronics_markup(cost)
-	# 	if cost.is_a?(Float) || cost.is_a?(Integer)
-	# 		if cost != nil
-	# 			(flat_markup(cost)*0.02).round(2)
-	# 		end
-	# 	else
-	# 		raise ArgumentError
-	# 	end
-	# end
-
-
-
-	def assert_valid_people_input(people)
-		if people.is_a?(Integer)
-			if people < 0
+	def assert_valid_people_input
+		if @number_of_people.is_a?(Integer)
+			if @number_of_people < 0
 				raise ArgumentError
 			else
-				people
+				@number_of_people
 			end
 		else
 			raise ArgumentError
 		end
 	end
 
-	def assert_valid_product_input(product)
-		if !product.is_a?(String)
-			raise ArgumentError
+	def assert_valid_product_input
+		if !@category.is_a?(String)
+			raise(ArgumentError, "Category must be a string")
 		else
-			product
+			@category
 		end
 	end
+
+  # def pharmaceuticals_markup(cost)
+  #   if cost.is_a?(Float) || cost.is_a?(Integer)
+  #     if cost != nil
+  #       (flat_markup(cost)*0.075).round(2)
+  #     end
+  #   else
+  #     raise(ArgumentError, "Must pass in price as a Float or Integer")
+  #   end
+  # end
+
+  # def food_markup(cost)
+  #   if cost.is_a?(Float) || cost.is_a?(Integer)
+  #     if cost != nil
+  #       (flat_markup(cost)*0.13).round(2)
+  #     end
+  #   else
+  #     raise ArgumentError
+  #   end
+  # end
+
+  # def electronics_markup(cost)
+  #   if cost.is_a?(Float) || cost.is_a?(Integer)
+  #     if cost != nil
+  #       (flat_markup(cost)*0.02).round(2)
+  #     end
+  #   else
+  #     raise ArgumentError
+  #   end
+  # end
+
+
+
 
 	def add_tax(product, people)
 		product_checker(product)
 		people_checker(people)
 		if product == "food"
-			0.13 + (for_each_person(people))
+			FOOD_RATE + (for_each_person(people))
 		elsif product == "drugs"
-			0.075 + (for_each_person(people))
+			DRUG_RATE + (for_each_person(people))
 		elsif product == "electronics"
-			0.02 + (for_each_person(people))
+			ELECTRONICS_RATE + (for_each_person(people))
 		else
 			for_each_person(people)
 		end
 	end
 
-	def markup_calc(base_price, num_people, category)
-		case category
-		when "food" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2) #removed puts since tests were reading the nil value as the returned value
-		when "drugs" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
-		when "electronics" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
-		when /.*/ then (flat_markup(base_price) * (1+add_tax(category, num_people))).round(2)
-		else puts "Not a valid input for category"
-		end
-	end
+	# def markup_calc(base_price, num_people, category)
+	# 	case @category
+	# 	when "food" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2) #removed puts since tests were reading the nil value as the returned value
+	# 	when "drugs" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
+	# 	when "electronics" then (flat_markup(base_price) * (1+add_tax(category,num_people))).round(2)
+	# 	when /.*/ then (flat_markup(base_price) * (1+add_tax(category, num_people))).round(2)
+	# 	else puts "Not a valid input for category"
+	# 	end
+	# end
+
+  def total_price
+    total_price_with_all_markups = (flat_markup + people_markup + category_markup).round(2)
+  end
 
 end
+
+product = Product.new(1299.99, 3, "food")
+puts product.total_price
+puts product.flat_markup
+puts product.category_markup
+puts product.people_markup
+product2=Product.new(5432, 1, "drugs")
+product3=Product.new(12456.95, 4, "books")
+puts "========================"
+puts product2.total_price
+puts product3.total_price
 # markup_calc(1299.99, 3, "food")
 # markup_calc(5432, 1, "drugs")
 # markup_calc(12456.95, 4, "books")
