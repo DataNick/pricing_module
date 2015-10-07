951afebeae6feb5bd44ce425bc303627eb28ecaa
DataNick <DataNick@users.noreply.github.com>
2015-10-07 17:19:14 -0400

    Added use case tests

--- tests/test_product.rb
+++ tests/test_product.rb
@@ -7,6 +7,14 @@ class TestSetup < Minitest::Test
     assert_equal 1591.58, Product.new(1299.99, 3, "food").total_price_with_markups
   end
 
+  def test_total_price_with_markups_equals_second_test_case
+    assert_equal 6199.81, Product.new(5432.00, 1, "drugs").total_price_with_markups
+  end
+
+  def test_total_price_with_markups_equals_third_test_case
+    assert_equal 13707.63, Product.new(12456.95, 4, "books").total_price_with_markups
+  end
+
   def test_price_cannot_be_string
     assert_raises ArgumentError do
       Product.new("1299.99", 3, "food")
